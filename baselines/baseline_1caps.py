2587861213ab26ee3baa50c7a8bd57c58b85df1b
iranroman <iran@ccrma.stanford.edu>
2022-03-01 20:11:45 -0500

    adds baseline measurements

---
+++ baselines/baseline_1caps.py
@@ -0,0 +1,115 @@
+import librosa
+import librosa.display
+import numpy as np
+import tensorflow as tf
+from itertools import chain, combinations
+from micarraylib.datasets import marco, eigenscape_raw
+from micarraylib.arraycoords.array_shapes_utils import _polar2cart
+import matplotlib.pyplot as plt
+import sys
+from copy import deepcopy
+from sklearn.neighbors import NearestNeighbors
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+rings = {
+    '1':[[5,6,2,3,4,12],[13,14,7,8,9,10,11,30,29],[21,28,27,15,16,32,31,23,22],[17,20,26,25,24,18],[19]],
+    '2':[[6,7,8,3,1],[5,14,15,9,4],[13,27,16,10,12],[29,28,26,32,11],[21,20,25,31,30],[17,19,24,23,22],[18]],
+}
+
+
+marco_dataset = marco(download=False, data_home='/home/iran/datasets/marco')
+#marco_dataset = eigenscape_raw(download=False, data_home='/home/iran/datasets/eigenscape_raw')
+marco_dataset.fs = 16000
+fs = marco_dataset.fs
+datapoint_dur = 0.1
+lr = 1e-6
+patience = 10
+edge = 0
+
+def extract_features(X, fs, datapoint_dur, nfft = 4096, hop_length=512):
+
+    X = np.array([[[x[i:i+int(datapoint_dur*fs)]] for i in range(0,len(x)-5*int(datapoint_dur*fs),int(datapoint_dur*fs))] for x in X]).transpose(1,2,3,0)
+    X = X/(np.max(X[:,edge:,edge:],axis=tuple(range(1,X.ndim)),keepdims=True)+1e-30)
+    return X
+
+mse = tf.losses.MeanSquaredError()
+
+# training and evaluation routine
+print("====================================")
+print("Evaluation after training")
+print("====================================")
+marco_data = {'Eigenmike':{}}#m:{} for m in marco_dataset.array_names}
+for array in marco_data.keys():
+    for ss in ['piano_solo_2']:
+        print('getting ', array, ' ', ss)
+        X = marco_dataset.get_audio_numpy(ss,array)
+
+        # extract features
+        marco_data[array][ss] = extract_features(X, fs, datapoint_dur)
+epoch_error = 0
+for array in marco_data.keys():
+
+    coords, capsule_names = marco_dataset.get_capsule_coords_numpy(array)
+    coords = np.array([v for v in _polar2cart({j:i for i,j in zip(coords,capsule_names)}, units='radians').values()])
+
+    source_error = 0
+    for ss in marco_data[array].keys():
+
+        data = marco_data[array][ss]
+        nchans = data.shape[-1]
+
+        in_size_error = 0
+        for in_size in [1]:
+
+            total_error = 0
+            for ichan in range(nchans):
+
+                ncombs = 1
+                all_chans_in = [rings[str(ichan+1)][3][0]]
+                all_chans_in = [[i-1 for i in all_chans_in]]
+                print(all_chans_in)
+                
+                comb_error = 0
+                for chans_in in all_chans_in:
+
+                    batch_size = 512
+                    batch_idx = np.random.choice(data.shape[0],batch_size,replace=False)
+                    X = data[batch_idx][...,np.array(chans_in)]
+                    Y = data[batch_idx][:,edge:,edge:,[ichan]]
+                    out = np.mean(X,axis=-1,keepdims=True)
+                    error = mse(out,Y)
+                    print(error)
+                    comb_error += error.numpy()
+                total_error += comb_error/ncombs 
+            fig, axs = plt.subplots(3, 1, figsize=(10, 5))
+            axs[0].plot(np.squeeze(Y[0]))
+            axs[0].axis(ymin=-1.0,ymax=1.0)
+            axs[0].grid(True)
+            axs[1].plot(np.squeeze(out[0]))
+            axs[1].axis(ymin=-1.0,ymax=1.0)
+            axs[1].grid(True)
+            axs[2].plot(np.abs(np.squeeze(out[0])-np.squeeze(Y[0])))
+            axs[2].axis(ymin=-1.0,ymax=1.0)
+            axs[2].grid(True)
+            plt.savefig('_'.join(['plots/eval_inference_mean',ss,array,str(in_size),'.png']))
+            plt.close()
+            Xerr = np.mean(np.squeeze(out-Y),axis=0)
+            plt.figure()
+            librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(Xerr, n_fft=256)), ref=np.max),sr=fs,hop_length=64,x_axis='time', y_axis='linear')
+            plt.colorbar()
+            plt.savefig('_'.join(['plots/eval_inference_mean_fft_error',ss,array,str(in_size),'.png']))
+            plt.close()
+            in_size_error += total_error/nchans 
+            print(array,ss,'No. input chans',in_size, 'MSE:', total_error/nchans)
+        source_error += in_size_error/len(range(nchans-1,nchans))
+
+    epoch_error += source_error/len(marco_data[array].keys())
+print('-----------------------------')
+print('                             ')
+print('Average error was', epoch_error/len(marco_data.keys()))
+print('                             ')
+print('-----------------------------')
