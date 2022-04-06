596a16ca98c06742b0e7a750cc681484381077dc
iranroman <iran@ccrma.stanford.edu>
2022-04-05 20:18:07 -0400

    training needs more RAM

--- tetra_eigenscape_dynfilts/unet_train.s
+++ tetra_eigenscape_dynfilts/unet_train.s
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=6-18:00:00
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --job-name=unet_varray
 #SBATCH --mail-type=END
 #SBATCH --output=slurm_%j.out
@@ -16,5 +16,5 @@ pwd
 
 nvidia-smi
 
-singularity exec --nv --overlay ../overlay-10GB-400K.ext3:ro /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif /bin/bash -c 'source /ext3/env.sh; python train.py'
+singularity exec --nv --overlay /scratch/irr2020/unet/overlay-10GB-400K.ext3:ro /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif /bin/bash -c 'source /ext3/env.sh; python train.py'
 
