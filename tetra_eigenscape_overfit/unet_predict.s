e1197435b724d145ac1322f34829c090d99eb258
iranroman <iran@ccrma.stanford.edu>
2022-04-11 11:31:52 -0400

    adds slurm script

--- tetra_eigenscape_overfit/unet_predict.s
+++ tetra_eigenscape_overfit/unet_predict.s
@@ -4,8 +4,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=16:00:00
-#SBATCH --mem=128GB
-#SBATCH --job-name=tetra_eigenscape_predict
+#SBATCH --mem=16GB
+#SBATCH --job-name=ovfit_tetra_predict
 #SBATCH --mail-type=END
 #SBATCH --output=slurm_%j.out
 #SBATCH --gres=gpu:1
@@ -16,4 +16,4 @@ pwd
 
 nvidia-smi
 
-singularity exec --nv --overlay ../overlay-10GB-400K.ext3:ro /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif /bin/bash -c 'source /ext3/env.sh; python predict.py'
+singularity exec --nv --overlay /scratch/irr2020/unet/overlay-10GB-400K.ext3:ro /scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif /bin/bash -c 'source /ext3/env.sh; python predict.py'
